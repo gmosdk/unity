2fb413a420c9430bb448bc6e3ec727f41b5fd313
tutv <tutv@appota.com>
2015-12-02 15:52:11 +0700

    Update iOS frameworks

--- Frameworks/GMOSDK.framework/Headers/GMOSDKConst.h
+++ Frameworks/GMOSDK.framework/Headers/GMOSDKConst.h
@@ -43,7 +43,7 @@ typedef void (^GMOSDKObjectHandler) (id object, NSError *error);
 //
 #define GMO_LANGUAGE_KEY [@"a0FwcG90YUxhbmdLZXk=" base64DecodedString]
 #define GMOGame_SDK_VERSION @"4.0"
-#define GMOGame_SDK_LABEL_VERSION @"4.0.9b6.9"
+#define GMOGame_SDK_LABEL_VERSION @"4.0.9b7.0"
 #define GMOGame_SDK_BUILD 40
 #define GMO_LOGIN_DICT_KEY_SAVED [@"a2V5X2FwcG90YV9sb2dpbl9kaWN0X3NhdmVk" base64DecodedString]
 
